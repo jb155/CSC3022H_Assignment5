e814ff6bbe380b3092e9aaabefb71eb085cc97d1
Jacques Botha <bthjac013@myUCT.ac.za>
2016-05-07 09:56:25 +0000

    Basic mono Audio stuffs done Except for the RMS and it's normalising...everything should be done for mono

--- src/Audio.h
+++ src/Audio.h
@@ -1,3 +1,4 @@
+
 #ifndef AUDIO_H
 #define AUDIO_H
 
@@ -41,11 +42,13 @@ namespace BTHJAC013{
 		//Destructor (Default)
 		~Audio() = default;
 		
+		//Operators
+		//********************************************************************************************
 		//Copy Assignment Operator
 		Audio &operator=(const Audio<T,1> & rhs){
-			Audio<T,1>temp(rhs);
-			*this = std::move(rhs);
-			return *this;
+			Audio<T,1>temp(rhs);			//Saw this on an online thread. Quite a nice shorthand...
+			*this = std::move(rhs);			//but I'm wondering waht effect ths will have on efficiency....
+			return *this;				//dont have time to worry bout that right now...but would be interesting to see.
 		}
 		
 		//Move Assignment Operator
@@ -64,7 +67,46 @@ namespace BTHJAC013{
 			return *this;
 		}
 		
+		//Pipe operator. Implements concatination
+		Audio &operator|(const Audio * rhs){
+			audioData.insert(audioData.end(), rhs.audioData.begin(), rhs.audioData.end());
+			return *this;
+		}
 		
+		//* operator. Multiplies L&R channel with number as given (Think of is as audio panning)
+		Audio &operator*(const std::pair <float, float> & volumeChange){
+			for (int i = 0; i < audioData.size(); i++){	//Step through of all audio data
+				audioData[i] *= volumeChange.first; 	//Seeing as this is only mono....only one channel data is needed
+			}
+			return *this;
+		}
+		
+		//+ Operator. Adds 2 audio samples of same size (same tSize and #Samples)
+		Audio &operator+(const Audio & rhs){
+			for (int i = 0; i < audioData.size(); i++){	//Step through all the audio data
+				audioData[i] = clampSampleMax(audioData[i] + rhs.audioData[i]);
+			}
+		}
+		
+		//^ Operator. Cut segment from audio sample.
+		Audio &operator^(const std::pair<int,int> & cutOutTimeSegment){
+			std::vector<T> resultingAudioSample;
+			
+			//Step through audioData and excl. data from the cutOutSegment
+			for (int i = 0; i < audioData.size(); i++){
+				if ((i < cutOutTimeSegment) || (i > cutOutSegment)){
+					resultingAudioSample.push_back(audioData[i]);
+				}
+			}
+			
+			//set current AudioData to new resulting data
+			audioData = resultingAudioSample;
+			
+			return *this;
+		}
+		
+		//Other fuctions
+		//******************************************************************************
 		//Read in file data from specified file (Handles the mono)
 		void readInFromFile (std::string inFileName){
 			std::ifstream infile(inFileName);
@@ -95,6 +137,59 @@ namespace BTHJAC013{
 			}
 			infile.close();
 		}
-	}
+		
+		//Write to file	(Mono)
+		void writeToFile (std::string outFileName){
+			//File name as stated in brief
+			std::string fileName = file + "_" + std::to_string(sampleRate) + "_" + std::to_string(tSize * 8) + "_mono.raw";
+			
+			std::ofstream outfile(fileName, std::ios::binary | std::ios::out);
+			
+			//checks if file is open (ie has been created) else, throws toys out of pram
+			if (outfile.is_open()){
+				outfile.write((char *)&audioData[0], audioData.size() * tSize);
+			}else{
+				std::cout << "Could not save file" << std::endl;
+			}
+			outfile.close();
+		}
+		
+		//Clamp sample Max. Adding two very loud files together may result in saturation.
+		T clampSampleMax(T audioSample){
+			if (tSize == 1){
+				return (sample > INT8_MAX) ? INT8_MAX : sample; 
+			}else{
+				return (sample > INT16_MAX) ? INT16_MAX : sample;
+			}
+		}
+		
+		//reverses audio data (essentially just rewrites the data back to front)
+		void reverse(){
+			std::reverse(audioData.begin(), audioData.end());
+		}
+		
+		//Select and return a specified segment of the audioData
+		Audio & selectRange(int rangeStart, int rangeEnd){
+			Audio * resultingAudioSampleObject = new Audio(*this);	//essentially make a duplicate of the current audio file
+			
+			resultingAudioSampleObject->audioData.clear();		//Clear out data stored within object
+			resultingAudioSampleObject->audioData.resize((unsigned long)(rangeEnd-rangeStart));	//Resize audioData to new range
+			
+			//was trying nested for loops and a ton of if statements....turns out...c++ has a convenient copy funct. Nice
+			//Copies audio data from start of the range to the end range into the new resulting audio sample 
+			std::copy(audioData.begin() + rangeStart, audioData.begin() + rangeEnd, resultingAudioSampleObject->audioData.begin());
+			
+			return *resultingAudioSampleObjectl;
+		}
+		
+		//returns the audio data within this object
+		std::vector <T> & getAudioData(){
+			return audioData;
+		}
+		
+		
+		
+        };
 }
+
 #endif
